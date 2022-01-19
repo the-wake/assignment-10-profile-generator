601e54397669806ddeaa852086226a581ee9df71
Ben Martin <bmartin2009@gmail.com>
2022-01-19 12:28:05 -0600

    Fixing bugs

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-// const inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee.js')
 const Manager = require('./lib/Manager.js')
+const Engineer = require('./lib/Engineer.js')
+const Intern = require('./lib/Intern.js')
 
 function init() {
-    addMngr()
-    // console.log(manager)
+    Manager.addMngr()
     .then((answers) => {
+        console.log(answers);
+        // Can destructure this
+        let projManager = new Manager.Manager (
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.office,
+        )
         console.log(projManager);
-        switch (answers.proceed) {
-            case 'Complete Entry':
+        switch (answers.next) {
+            case 'Engineer':
+                Engineer.addEngi()
+                .then((answers) => {
+                    let projEngi = new Engineer.Engineer (
+                        answers.name,
+                        answers.id,
+                        answers.email,
+                        answers.gitHub,
+                    )
+                    console.log(projEngi)
+                });
                 break;
-            case 'Add an Engineer':
-                addEngi();
+            case 'Intern':
+                Intern.addIntern()
+                .then((answers) => {
+                    let projIntern = new Intern.Intern (
+                        answers.name,
+                        answers.id,
+                        answers.email,
+                        answers.gitHub,
+                    )
+                    console.log(projIntern);
+                });
                 break;
-            case 'Add an Intern':
-                addIntern();
+            case 'No - Finish Setup':
                 break;
         }
     })
